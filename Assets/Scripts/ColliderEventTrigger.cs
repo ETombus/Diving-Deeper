a7c005d8dbcd53ed18f7a7c2f0c07b8e4ac90ddf
Tom <tom.eckerstrom@hotmail.com>
2023-08-27 00:22:58 +0200

    coolt

--- Assets/Scripts/ColliderEventTrigger.cs
+++ Assets/Scripts/ColliderEventTrigger.cs
@@ -3,12 +3,13 @@ using UnityEngine.Events;
 
 public class ColliderEventTrigger : MonoBehaviour
 {
-    private string targetTag = "Player"; // The tag to compare with
+    private string targetTag = "Sub"; // The tag to compare with
     public UnityEvent enterEvent; // The event to trigger on enter
     public UnityEvent exitEvent; // The event to trigger on exit
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log(other.name);
         if (other.CompareTag(targetTag))
         {
             enterEvent.Invoke(); // Trigger the enter event
