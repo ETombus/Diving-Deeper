cffd3f5b2505510a1ad7396f34c28abbd9341b8f
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-08-26 18:11:58 +0200

    added diver stuff

---
+++ Assets/Scripts/Diver.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Diver : MonoBehaviour
+{
+    [SerializeField] private Submarine subScript;
+    [SerializeField] private GameObject visibleDiver;
+    [SerializeField] private GameObject visibility;
+
+    private Camera cam;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public SpriteRenderer spriteRenderer;
+
+    public int points = 0;
+
+    private Vector2 rotation;
+    private float maxVelocity = 1.5f;
+    private float rotationSpeed = 5f;
+
+    private bool inSubRange;
+
+    void Start()
+    {
+        cam = Camera.main;
+
+        rb = GetComponent<Rigidbody2D>();
+
+        animator = visibleDiver.GetComponent<Animator>();
+        spriteRenderer = visibleDiver.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!subScript.isDead && subScript.isSwimming)
+        {
+            rotation.x = Input.GetAxisRaw("Horizontal");
+            rotation.y = Input.GetAxisRaw("Vertical");
+
+            if (rotation.x != 0 || rotation.y != 0)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, rotation);
+                visibleDiver.transform.rotation = Quaternion.Slerp(visibleDiver.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKeyDown("space"))
+            {
+                Vector2 force = (visibleDiver.transform.rotation * Vector2.up) * 3f;
+                rb.AddForce(force, ForceMode2D.Force);
+            }
+
+            float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+            float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+            rb.velocity = new Vector2(x, y);
+
+
+            if (Input.GetKeyDown(KeyCode.E) && inSubRange && !subScript.justSwapped)
+            {
+                subScript.justSwapped = true;
+                subScript.isSwimming = false;
+                spriteRenderer.enabled = false;
+                subScript.SetSubCamera();
+                rb.velocity = Vector3.zero;
+                Invoke(nameof(ResetJustSwappedBool), 1f);
+
+            }
+        }
+    }
+
+    public void SetDiverCamera()
+    {
+        cam.transform.parent = transform;
+        visibility.transform.parent = transform;
+        StartCoroutine(LerpCameraSize());
+    }
+
+    private IEnumerator LerpCameraSize()
+    {
+        float elapsedTime = 0f;
+        float lerpDuration = 0.3f;
+
+        while (elapsedTime < lerpDuration)
+        {
+            float t = Mathf.Clamp01(elapsedTime / lerpDuration);
+
+            float newSize = Mathf.Lerp(5, 0.5f, t);
+
+            cam.orthographicSize = newSize;
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null; // Wait for the next frame
+        }
+
+        cam.orthographicSize = 0.5f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectable"))
+        {
+            Collect(collision.gameObject);
+            collision.gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.CompareTag("Sub"))
+        {
+            inSubRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Sub"))
+        {
+            inSubRange = false;
+        }
+    }
+
+    private void Collect(GameObject collectable)
+    {
+        points++;
+    }
+
+    private void ResetJustSwappedBool()
+    {
+        subScript.justSwapped = false;
+    }
+}
--- Assets/Scripts/Submarine.cs
+++ Assets/Scripts/Submarine.cs
@@ -1,43 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Submarine : MonoBehaviour
 {
-    [SerializeField] private GameObject bubbles;
+    [SerializeField] private Diver diverScript;
+    [SerializeField] private GameObject visibility;
 
+    private Camera cam;
     private Rigidbody2D rb;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     private Vector2 dir;
     private float speed = 5;
     private float maxVelocity = 15f;
 
     public bool isDead;
+    public bool isSwimming;
+    public bool justSwapped;
 
     private void Start()
     {
+        cam = Camera.main;
+
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        if (!isDead)
+        if (!isDead && !isSwimming)
         {
             dir.x = Input.GetAxisRaw("Horizontal");
             dir.y = Input.GetAxisRaw("Vertical");
 
             rb.velocity += dir * speed * Time.deltaTime;
-
             ClampVelocity();
+
+            if (dir.x < 0)
+                spriteRenderer.flipX = false;
+            else if (dir.x > 0)
+                spriteRenderer.flipX = true;
+
+            if (Input.GetKeyDown(KeyCode.E) && !justSwapped)
+            {
+                justSwapped = true;
+                isSwimming = true;
+                diverScript.gameObject.transform.position = transform.position;
+                diverScript.spriteRenderer.enabled = true;
+                diverScript.SetDiverCamera();
+
+                Invoke(nameof(ResetJustSwappedBool), 1f);
+            }
+
         }
     }
 
     private void ClampVelocity()
     {
-        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
-        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
-        rb.velocity = new Vector2(x, y);
+        dir.x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        dir.y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+        rb.velocity = dir;
+    }
+
+    public void SetSubCamera()
+    {
+        visibility.transform.parent = transform;
+
+        cam.transform.parent = transform;
+        cam.transform.localPosition = new Vector3(0, 0, -10);
+        StartCoroutine(LerpCameraSize());
+    }
+
+    private IEnumerator LerpCameraSize()
+    {
+        float elapsedTime = 0f;
+        float lerpDuration = 0.3f;
+
+        while (elapsedTime < lerpDuration)
+        {
+            float t = Mathf.Clamp01(elapsedTime / lerpDuration);
+
+            float newSize = Mathf.Lerp(0.5f, 5, t);
+
+            cam.orthographicSize = newSize;
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null; // Wait for the next frame
+        }
+
+        cam.orthographicSize = 5;
+    }
+
+    private void ResetJustSwappedBool()
+    {
+        justSwapped = false;
     }
 }
